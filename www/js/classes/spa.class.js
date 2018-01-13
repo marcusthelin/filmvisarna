07ab6a05f80ca5858835724c973db4e298dfe1e3
Markus Andersson <make_99@hotmail.com>
2018-01-13 13:32:07 +0100

    Added Filmer skeleton and cleaned the navbar

--- www/js/classes/spa.class.js
+++ www/js/classes/spa.class.js
@@ -2,7 +2,7 @@ class PopStateHandler {
 
   // Note: Only instantiate PopStateHandler once!
 
-  constructor(){
+  constructor() {
     // Add event handlers for a.pop-links once
     this.addEventHandler();
     // Call changePage on initial page load
@@ -15,13 +15,13 @@ class PopStateHandler {
 
   }
 
-  addEventHandler(){
+  addEventHandler() {
 
     // make "that" the PopStateHandler object
     // (since this will be the a tag inside the click function)
     let that = this;
 
-    $(document).on('click','a.pop',function(e){
+    $(document).on('click', 'a.pop', function(e) {
 
       // Create a push state event
       let href = $(this).attr('href');
@@ -36,7 +36,7 @@ class PopStateHandler {
     });
   }
 
-  changePage(){
+  changePage() {
     // React on page changed
     // (replace part of the DOM etc.)
 
@@ -63,10 +63,10 @@ class PopStateHandler {
 
   }
 
-  startsida(){
+  startsida() {
     $('body').html(`
       <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" role="navigation">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark" role="navigation">
           <div class="container">
             <img class="navbar-brand" href="#">
             <button class="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
@@ -81,7 +81,6 @@ class PopStateHandler {
                 <li class="nav-item"><a href="/omOss" class="nav-link">Om oss</a></li>
               </ul>
               <ul class="nav navbar-nav flex-row justify-content-between ml-auto">
-                <li class="nav-item order-2 order-md-1"><a href="#" class="nav-link" title="settings"><i class="fa fa-cog fa-fw fa-lg"></i></a></li>
                 <li class="dropdown order-1">
                   <button type="button" id="dropdownMenu1" data-toggle="dropdown" class="btn btn-outline-secondary dropdown-toggle">Login <span class="caret"></span></button>
                   <ul class="dropdown-menu dropdown-menu-right mt-1">
@@ -214,10 +213,10 @@ class PopStateHandler {
     `);
   }
 
-  filmer(){
+  filmer() {
     $('body').html(`
       <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" role="navigation">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark" role="navigation">
           <div class="container">
             <img class="navbar-brand" href="#">
             <button class="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
@@ -232,7 +231,139 @@ class PopStateHandler {
                 <li class="nav-item"><a href="/omOss" class="nav-link">Om oss</a></li>
               </ul>
               <ul class="nav navbar-nav flex-row justify-content-between ml-auto">
-                <li class="nav-item order-2 order-md-1"><a href="#" class="nav-link" title="settings"><i class="fa fa-cog fa-fw fa-lg"></i></a></li>
+                <li class="dropdown order-1">
+                  <button type="button" id="dropdownMenu1" data-toggle="dropdown" class="btn btn-outline-secondary dropdown-toggle">Login <span class="caret"></span></button>
+                  <ul class="dropdown-menu dropdown-menu-right mt-1">
+                    <li class="p-3">
+                      <form class="form" role="form">
+                        <div class="form-group">
+                          <input id="emailInput" placeholder="Användarnamn" class="form-control form-control-sm" type="text" required="">
+                        </div>
+                        <div class="form-group">
+                          <input id="passwordInput" placeholder="Lösenord" class="form-control form-control-sm" type="text" required="">
+                        </div>
+                        <div class="form-group">
+                          <button type="submit" class="btn btn-primary btn-block">Logga in</button>
+                        </div>
+                      </form>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header>
+      <main>
+        <div class="container">
+          <div class="row col-lg-12 mt-2">
+            <div class="col-lg-8">
+              <div class="media border">
+                <img class="d-flex mr-3 img-fluid" src="/imgs/test2.jpg" alt="Generic placeholder image">
+                <div class="media-body">
+                  <h5 class="mt-0">Media heading Film 1</h5>
+                  Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                </div>
+              </div>
+              <div class="media border mt-2">
+                <img class="d-flex mr-3 img-fluid" src="/imgs/test2.jpg" alt="Generic placeholder image">
+                <div class="media-body">
+                  <h5 class="mt-0">Media heading film 2</h5>
+                  Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                </div>
+              </div>
+              <div class="media border mt-2">
+                <img class="d-flex mr-3 img-fluid" src="/imgs/test2.jpg" alt="Generic placeholder image">
+                <div class="media-body">
+                  <h5 class="mt-0">Media heading film 3</h5>
+                  Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                </div>
+              </div>
+              <div class="media border mt-2">
+                <img class="d-flex mr-3 img-fluid" src="/imgs/test2.jpg" alt="Generic placeholder image">
+                <div class="media-body">
+                  <h5 class="mt-0">Media heading film 4</h5>
+                  Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                </div>
+              </div>
+              <div class="media border mt-2">
+                <img class="d-flex mr-3 img-fluid" src="/imgs/test2.jpg" alt="Generic placeholder image">
+                <div class="media-body">
+                  <h5 class="mt-0">Media heading film 5</h5>
+                  Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                </div>
+              </div>
+              <div class="media border mt-2">
+                <img class="d-flex mr-3 img-fluid" src="/imgs/test2.jpg" alt="Generic placeholder image">
+                <div class="media-body">
+                  <h5 class="mt-0">Media heading film 6</h5>
+                  Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                </div>
+              </div>
+
+            </div>
+            <div class="col-lg-4">
+              <div class="kalendarium border">
+                <p>Kalendarium</p>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </main>
+      <footer class="container-fluid bg-light">
+        <section class="row mt-3 mb-3">
+          <div class="col-lg-4 py-2">
+            <div class="row justify-content-center">
+              <div class="col-11 col-md-7 col-lg-12 col-xl-10 color d-flex justify-content-center align-items-center myheight">
+                <p class="mb-1"><span class="fa fa-address-card mr-2"></span><span class="d-none d-lg-inline">Adress:</span></p>
+                <p class="ml-5 pl-2">
+                  Fyrairadgatan 4<br> Fyrgatan 44<br> 44 044 Malmö
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 py-2 ">
+            <div class="row justify-content-center">
+              <div class="col-11 col-md-7 col-lg-12 col-xl-10 color d-flex justify-content-center align-items-center myheight">
+                <div>
+                  <p class="mb-1 pr-2"><span class="fa fa-facebook mr-3"></span><a href="#">Möt oss på Facebook!</span></a></p>
+                  <p class="mb-1 pr-2"><span class="fa fa-envelope mr-3"> </span><a href="#">us@fourinarow.se</a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 py-2 ">
+            <div class="row justify-content-center">
+              <div class="col-11 col-md-7 col-lg-11 col-xl-10 color d-flex justify-content-center align-items-center myheight">
+                <p class="mb-1 pr-2"><span class="fa fa-phone mr-3"></span>Telefon: 040-44 33 44</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </footer>
+      `);
+  }
+
+  boka() {
+    $('body').html(`
+      <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark" role="navigation">
+          <div class="container">
+            <img class="navbar-brand" href="#">
+            <button class="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
+                &#9776;
+            </button>
+            <div class="collapse navbar-collapse" id="exCollapsingNavbar">
+              <ul class="nav navbar-nav">
+                <li class="nav-item"><a href="/" class="nav-link">Startsida</a></li>
+                <li class="nav-item"><a href="/filmer" class="nav-link">Filmer</a></li>
+                <li class="nav-item"><a href="/boka" class="nav-link">Boka</a></li>
+                <li class="nav-item"><a href="/biljetter" class="nav-link">Biljetter</a></li>
+                <li class="nav-item"><a href="/omOss" class="nav-link">Om oss</a></li>
+              </ul>
+              <ul class="nav navbar-nav flex-row justify-content-between ml-auto">
                 <li class="dropdown order-1">
                   <button type="button" id="dropdownMenu1" data-toggle="dropdown" class="btn btn-outline-secondary dropdown-toggle">Login <span class="caret"></span></button>
                   <ul class="dropdown-menu dropdown-menu-right mt-1">
@@ -293,10 +424,10 @@ class PopStateHandler {
       `);
   }
 
-  boka(){
+  biljetter() {
     $('body').html(`
       <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" role="navigation">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark" role="navigation">
           <div class="container">
             <img class="navbar-brand" href="#">
             <button class="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
@@ -311,7 +442,6 @@ class PopStateHandler {
                 <li class="nav-item"><a href="/omOss" class="nav-link">Om oss</a></li>
               </ul>
               <ul class="nav navbar-nav flex-row justify-content-between ml-auto">
-                <li class="nav-item order-2 order-md-1"><a href="#" class="nav-link" title="settings"><i class="fa fa-cog fa-fw fa-lg"></i></a></li>
                 <li class="dropdown order-1">
                   <button type="button" id="dropdownMenu1" data-toggle="dropdown" class="btn btn-outline-secondary dropdown-toggle">Login <span class="caret"></span></button>
                   <ul class="dropdown-menu dropdown-menu-right mt-1">
@@ -372,10 +502,10 @@ class PopStateHandler {
       `);
   }
 
-  biljetter(){
+  omOss() {
     $('body').html(`
       <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" role="navigation">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark" role="navigation">
           <div class="container">
             <img class="navbar-brand" href="#">
             <button class="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
@@ -390,86 +520,6 @@ class PopStateHandler {
                 <li class="nav-item"><a href="/omOss" class="nav-link">Om oss</a></li>
               </ul>
               <ul class="nav navbar-nav flex-row justify-content-between ml-auto">
-                <li class="nav-item order-2 order-md-1"><a href="#" class="nav-link" title="settings"><i class="fa fa-cog fa-fw fa-lg"></i></a></li>
-                <li class="dropdown order-1">
-                  <button type="button" id="dropdownMenu1" data-toggle="dropdown" class="btn btn-outline-secondary dropdown-toggle">Login <span class="caret"></span></button>
-                  <ul class="dropdown-menu dropdown-menu-right mt-1">
-                    <li class="p-3">
-                      <form class="form" role="form">
-                        <div class="form-group">
-                          <input id="emailInput" placeholder="Användarnamn" class="form-control form-control-sm" type="text" required="">
-                        </div>
-                        <div class="form-group">
-                          <input id="passwordInput" placeholder="Lösenord" class="form-control form-control-sm" type="text" required="">
-                        </div>
-                        <div class="form-group">
-                          <button type="submit" class="btn btn-primary btn-block">Logga in</button>
-                        </div>
-                      </form>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </header>
-      <main>
-      </main>
-      <footer class="container-fluid bg-light">
-        <section class="row mt-3 mb-3">
-          <div class="col-lg-4 py-2">
-            <div class="row justify-content-center">
-              <div class="col-11 col-md-7 col-lg-12 col-xl-10 color d-flex justify-content-center align-items-center myheight">
-                <p class="mb-1"><span class="fa fa-address-card mr-2"></span><span class="d-none d-lg-inline">Adress:</span></p>
-                <p class="ml-5 pl-2">
-                  Fyrairadgatan 4<br> Fyrgatan 44<br> 44 044 Malmö
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-4 py-2 ">
-            <div class="row justify-content-center">
-              <div class="col-11 col-md-7 col-lg-12 col-xl-10 color d-flex justify-content-center align-items-center myheight">
-                <div>
-                  <p class="mb-1 pr-2"><span class="fa fa-facebook mr-3"></span><a href="#">Möt oss på Facebook!</span></a></p>
-                  <p class="mb-1 pr-2"><span class="fa fa-envelope mr-3"> </span><a href="#">us@fourinarow.se</a></p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-4 py-2 ">
-            <div class="row justify-content-center">
-              <div class="col-11 col-md-7 col-lg-11 col-xl-10 color d-flex justify-content-center align-items-center myheight">
-                <p class="mb-1 pr-2"><span class="fa fa-phone mr-3"></span>Telefon: 040-44 33 44</span>
-                </p>
-              </div>
-            </div>
-          </div>
-        </section>
-      </footer>
-      `);
-  }
-
-  omOss(){
-    $('body').html(`
-      <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" role="navigation">
-          <div class="container">
-            <img class="navbar-brand" href="#">
-            <button class="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
-                &#9776;
-            </button>
-            <div class="collapse navbar-collapse" id="exCollapsingNavbar">
-              <ul class="nav navbar-nav">
-                <li class="nav-item"><a href="/" class="nav-link">Startsida</a></li>
-                <li class="nav-item"><a href="/filmer" class="nav-link">Filmer</a></li>
-                <li class="nav-item"><a href="/boka" class="nav-link">Boka</a></li>
-                <li class="nav-item"><a href="/biljetter" class="nav-link">Biljetter</a></li>
-                <li class="nav-item"><a href="/omOss" class="nav-link">Om oss</a></li>
-              </ul>
-              <ul class="nav navbar-nav flex-row justify-content-between ml-auto">
-                <li class="nav-item order-2 order-md-1"><a href="#" class="nav-link" title="settings"><i class="fa fa-cog fa-fw fa-lg"></i></a></li>
                 <li class="dropdown order-1">
                   <button type="button" id="dropdownMenu1" data-toggle="dropdown" class="btn btn-outline-secondary dropdown-toggle">Login <span class="caret"></span></button>
                   <ul class="dropdown-menu dropdown-menu-right mt-1">
