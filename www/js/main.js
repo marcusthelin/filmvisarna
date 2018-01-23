778bc4c60a2a400a56a8ab505b9f553e06d7867d
Markus Andersson <make_99@hotmail.com>
2018-01-23 08:58:13 +0100

    Small buggfix with loading in the JSON

--- www/js/main.js
+++ www/js/main.js
@@ -1,10 +1,11 @@
 let spa;
 //JSON._load('comingMovies.json');
-JSON._load('movies.json').then(function(movie){
-  JSON._load('comingMovies.json').then(function(comingMovie){
-    Templates.comingMovies = comingMovie;
+JSON._load('comingMovies.json').then(function(comingMovie){
+  Templates.comingMovies = comingMovie;
 
-  });
+});
+JSON._load('movies.json').then(function(movie){
+
   Templates.movies = movie;
   // Templates.comingMovies = comingMovies;
   spa = new Spa();
