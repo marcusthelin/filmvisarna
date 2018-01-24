dece24f01b74a928ff7f9577353828bc71ad828c
Marcus Thelin <marcusthelin97@gmail.com>
2018-01-24 14:49:42 +0100

    In progress

--- www/js/classes/movie.class.js
+++ www/js/classes/movie.class.js
@@ -1,6 +1,28 @@
 class Movie extends Base {
   constructor(){
     super();
+    this.movies = [];
+  }
+
+  gatherMovies(){
+
+
+    let date = new Date();
+
+    date = date.getFullYear()+'-0'+(date.getMonth()+1)+'-'+date.getDate()
+
+
+
+    let i = 0;
+    for(i; i < this.app.showtime.length; i++){
+      if(this.app.showtime[i].film == this.title && this.app.showtime[i].date == date){
+        for(let x = i; x < this.app.showtime.length; x++){
+          if(this.app.showtime[x].film == this.title){
+            this.movies.push(this.app.showtime[x])
+          }
+        }
+      }
+    }
   }
 
 }
