dd51e42c5c7aeabade10628bda8e02f629931237
Marcus Thelin <marcusthelin97@gmail.com>
2018-01-26 09:49:06 +0100

    Fixed fading carousel and so it works on page load

--- www/js/classes/startsidan.class.js
+++ www/js/classes/startsidan.class.js
@@ -2,5 +2,12 @@ class Startsidan extends Base {
 	constructor(app){
 		super();
 		this.app = app;
+		this.callCarousel();
+	}
+
+	callCarousel(){
+		$(document).ready(function() {
+			$('#carouselExampleControls').carousel('cycle');
+		});
 	}
 }
