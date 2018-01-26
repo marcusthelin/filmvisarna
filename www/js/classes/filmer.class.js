a0055eaca4d9cba37e2019b2580229323a68bad8
Markus Andersson <make_99@hotmail.com>
2018-01-26 09:27:54 +0100

    Revert "Merge branch 'feature/color-experiment' into develop"
    This reverts commit 7b9dd440fca3f4708a4faf21ef01177242574f39.

--- www/js/classes/filmer.class.js
+++ www/js/classes/filmer.class.js
@@ -3,19 +3,18 @@ class Filmer extends Base {
 		super();
 		this.app = app;
 		this.getMovies(new Date());
-		this.format = new Intl.DateTimeFormat("sv", {weekday:"long", day:"numeric", month: "long" });
 	}
 
 
 	getMovies(date) {
-		this.schedule = [];
-		for(let days = 0; days< 7; days++){
-			this.movies = [this.app.showtime.filter(movie => movie.date == date.getFullYear()+'-0'+(date.getMonth()+1)+'-'+(date.getDate()+days))];
-			for(let i = 0; i< this.movies.length; i++ ){
-				this.schedule.push(this.movies[i]);
-			}
-			this.movies = [];
-		}
+		this.todayMovies = [];
+		this.tomorrowMovies = [];
+		this.dayAfterTomorrow = [];
+
+    this.todayMovies = this.app.showtime.filter(movie => movie.date == date.getFullYear()+'-0'+(date.getMonth()+1)+'-'+date.getDate());
+    this.tomorrowMovies = this.app.showtime.filter(movie => movie.date == date.getFullYear()+'-0'+(date.getMonth()+1)+'-'+(date.getDate()+1));
+		this.dayAfterTomorrow = this.app.showtime.filter(movie => movie.date == date.getFullYear()+'-0'+(date.getMonth()+1)+'-'+(date.getDate()+2));
+
   }
 
 }
