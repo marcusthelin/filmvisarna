ea2c28ea78cfc656b70ca6f9d1620d98520be4fc
Marcus Thelin <marcusthelin97@gmail.com>
2018-01-30 15:57:44 +0100

    Fixed json save!

--- www/js/classes/register.class.js
+++ www/js/classes/register.class.js
@@ -2,6 +2,11 @@ class Register extends Base{
 	constructor(){
 		super();
 		this.checkClick();
+		this.load();
+	}
+
+	async load(){
+		this.users = await JSON._load('users.json');
 	}
 
 	drawRegisterModal(){
@@ -10,13 +15,43 @@ class Register extends Base{
 		$('#register-modal').modal();
 	}
 
-	
+
 	checkClick(){
+		let that = this;
 		$(document).on('click', '.register', function(){
 			let username = $('#name').val();
 			let password = $('#password').val();
-			console.log(username, password);
+
+			let newUser = {username: username, password: password};
+
+			that.users.push(newUser);
+
+			JSON._save('users.json', that.users);
+
+			console.log(that.users);
+
+
+
+
+
+			// console.log(username, password);
+			// console.log(this.users);
 		});
+
+
 	}
+
+
+	// save(data){
+	// 	this.users.push({
+	// 		username: data.username,
+	// 		password: data.password
+	// 	});
+	// 	JSON._save('users.json', {
+	// 		users: this.users;
+	// 	});
+  //
+	// 	console.log(this.users);
+	// }
 
 }
