186055077110c0d2262e4c20da83a802f55bd9a5
Shamin Krook <shahmineh.gh@hotmail.com>
2018-02-01 14:09:17 +0100

    Fixed design for modal body

--- www/js/classes/seat.class.js
+++ www/js/classes/seat.class.js
@@ -8,7 +8,7 @@ class Seat {
   htmlSeatTemplate(x, y) {
     return `
       <rect x="${x}" y="${y}" rx="7" ry="7" width="30" height="30"
-      style="fill:black;stroke:black;stroke-width:2;opacity:0.3" />
+      style="fill:white;stroke:white;stroke-width:2;opacity:0.3" />
     `
   }
 }
