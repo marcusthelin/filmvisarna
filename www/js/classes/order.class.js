a48384d977e37726b858bf0bac6ebd150677c87e
Markus Andersson <make_99@hotmail.com>
2018-02-07 15:06:47 +0100

    more work in progress

---
+++ www/js/classes/order.class.js
@@ -0,0 +1,11 @@
+class Order extends Base {
+  constructor(orderNr, title, mNr, date, salong) {
+    super();
+    this.orderNr = orderNr;
+    this.title = title
+    this.mNr = mNr
+    this.date = date
+    this.salong = salong;
+  }
+  
+}
