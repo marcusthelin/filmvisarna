98d529830ab0911bb2c4caf9b99648d11f5ca47b
Marcus Thelin <marcusthelin97@gmail.com>
2018-02-09 00:39:52 +0100

    Made it possible to cancel booking
    REMEMBER TO LOOK AT LINE 53 in filmer.html!

--- www/js/classes/user-page.class.js
+++ www/js/classes/user-page.class.js
@@ -1,23 +1,28 @@
 class UserPage extends Base{
 	constructor(){
 		super();
-		this.checkClickLogout();
+		this.clickEvents();
 	}
 
 	/* When clicking logout, set the value in session.json to 0
 	and change page to Startsida */
-	checkClickLogout(){
-
+	clickEvents(){
 		$(document).on('click', '.logoutModalBtn', function(){
 			console.log('Clicked');
 			$('#exampleModal').appendTo('body').modal('show');
 		});
 
-		$(document).on('click', '.logout', function(e){
+		$(document).on('click', '.logout', function(){
 			let session = JSON._load('session');
 			session = 0;
 			JSON._save('session', session);
 			$('#exampleModal').modal('hide');
+		});
+
+		$(document).on('click', '.cancel-order', () => {
+			let orderNumber = $('.cancel-order').parent().find('ul').find(':first').text().slice(13); //Just get the ordernumber from card
+			console.log(orderNumber);
+			this.cancelOrder(orderNumber);
 		});
 	}
 
@@ -71,7 +76,7 @@ class UserPage extends Base{
 			} 
 				//If booking's viewdate has past, save the order object to
 				//order-history.json and remove it from order.json
-				else if(viewDate < today){
+				else if((viewDate < today) || (viewDate != today)){
 				this.historyBookings.unshift(order);
 				index = userOrders.indexOf(order);
 				userOrders.splice(index, 1);
@@ -91,31 +96,44 @@ class UserPage extends Base{
 	}
 
 	renderCurrentBookings(currentUser){
-		let userCurrentBookings = [];
+		this.userCurrentBookings = [];
 
 		this.currentBookings.filter(booking => {
 			if(booking.orderInfo.mNr == currentUser){
-				userCurrentBookings.unshift(booking);
+				this.userCurrentBookings.unshift(booking);
 			}
 		});
 
-		if(!(userCurrentBookings.length > 0)){
+		if(!(this.userCurrentBookings.length > 0)){
+			$('.aktuella').empty();
 			$('#aktuella-heading').after('<h3>Du har inga aktuella bokningar.');	
-		} else userCurrentBookings.render('.aktuella', '2');
+		} else this.userCurrentBookings.render('.aktuella', '2');
 	}
 
 	async renderHistoryBookings(currentUser){
 		let oldBookings = await JSON._load('order-history');
 		let userOldBookings = [];
+		$('.order-history').find('h3').remove();
 
 		oldBookings.filter(booking => {
 			if(booking.orderInfo.mNr == currentUser){
-				userOldBookings.unshift(booking);
+				userOldBookings.push(booking);
 			}
 		});
 
 		if(!(userOldBookings.length > 0)){
 			$('.order-history > h2').after('<h3>Du har ingen historik.');
 		} else userOldBookings.render('.order-history-list');
+	}
+
+	cancelOrder(orderNumber){
+		let index;
+		this.currentBookings.filter(order => {
+			if(order.orderNr == orderNumber){
+				index = this.currentBookings.indexOf(order);
+				console.log(this.currentBookings.splice(index, 1));
+				JSON._save('orders', this.currentBookings.splice(index, 1)).then(() => this.filterOrdersByDate());
+			}
+		});
 	}
 }
